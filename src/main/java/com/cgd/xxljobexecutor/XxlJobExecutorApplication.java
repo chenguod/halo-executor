ae4d29b98a3dba3857aa32be6b324e97d855c0e8
chengd <1475313408@qq.com>
2021-05-18 17:08:37 +0800

    fix:搭建架子

---
+++ src/main/java/com/cgd/xxljobexecutor/XxlJobExecutorApplication.java
@@ -0,0 +1,13 @@
+package com.cgd.xxljobexecutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxlJobExecutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobExecutorApplication.class, args);
+    }
+
+}
