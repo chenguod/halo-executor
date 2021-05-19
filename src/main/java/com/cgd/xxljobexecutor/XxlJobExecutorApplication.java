825325d2c1d1ed12a5d97214e1b166874adc3061
chengd <1475313408@qq.com>
2021-05-19 16:31:58 +0800

    fix:增加测试任务，引入xxlJob

--- src/main/java/com/cgd/xxljobexecutor/XxlJobExecutorApplication.java
+++ src/main/java/com/cgd/xxljobexecutor/XxlJobExecutorApplication.java
@@ -10,5 +10,4 @@ public class XxlJobExecutorApplication {
     public static void main(String[] args) {
         SpringApplication.run(XxlJobExecutorApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/cgd/xxljobexecutor/xxlJob/PushJob.java
@@ -0,0 +1,23 @@
+package com.cgd.xxljobexecutor.xxlJob;
+
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author chengd
+ * @version 1.0
+ * @date 2021/5/19 15:24
+ */
+@Component
+public class PushJob {
+
+    @XxlJob("PushJobHandler")
+    public ReturnT<String> PushJobHandler(String param){
+        ReturnT<String> returnT = new ReturnT<>();
+        returnT.setCode(ReturnT.SUCCESS_CODE);
+        returnT.setContent("test");
+        returnT.setMsg("<p style=\"color:red\">test</p>");
+        return returnT;
+    }
+}
